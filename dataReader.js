1ae7e0b42c978f9e07fdbf127b9f9b6a9fded95a
anchal-lnmiit <anchal-lnmiit@users.noreply.github.com>
2023-02-12 13:59:41 +0530

    Add files via upload

---
+++ dataReader.js
@@ -0,0 +1,30 @@
+
+var data = [
+    {
+    "Name":"Anchal Ag",
+    "Position":1,
+    "StepsTod":3000,
+    "TotalSteps":8000
+},
+
+{
+    "Name":"Vidya",
+    "Position":3,
+    "StepsTod":2000,
+    "TotalSteps":9000
+},
+{
+    "Name":"Aruna",
+    "Position":2,
+    "StepsTod":4000,
+    "TotalSteps":20000
+}
+];
+document.getElementById("test").innerHTML=data[0].Name;
+//document.body.innerHTML="Anchal";
+//console.log(dataJson.data[1].Name);
+//  console.log(mydata[0].Name);
+//  console.log(mydata[0].Position);
+//  console.log(mydata[1].Name);
+//  console.log(mydata[1].StepsTod);
+
